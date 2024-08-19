31dd6b9d83081eaf581d3e4a64e552cb3013d4a9
Laiz Detros <megami.ay@gmail.com>
2024-08-19 20:41:10 -0300

    Create Principal.java

---
+++ exercicio /ex03 /Principal.java
@@ -0,0 +1,12 @@
+// numeros de 50 a 100. pega
+
+package ex03;
+
+public class Principal { 
+
+    public static void main(String[] args) {
+        for (int num = 50; num <= 100; num++) {
+            System.out.println(num);
+        }
+    }
+}
