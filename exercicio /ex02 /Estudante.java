212764bd3a62c07be705c4955faa3b92b7395c90
Laiz Detros <megami.ay@gmail.com>
2024-08-19 20:20:41 -0300

    Create Estudante.java

---
+++ exercicio /ex02 /Estudante.java
@@ -0,0 +1,20 @@
+package teste;
+
+public class Estudante {
+	String nomeEstudante;
+	String cursoMatriculado;
+	int idadeEstudante;
+	
+	public Estudante() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Estudante [nomeEstudante=" + nomeEstudante + ", cursoMatriculado=" + cursoMatriculado
+				+ ", idadeEstudante=" + idadeEstudante + "]";
+	}
+	
+	
+	
+}
