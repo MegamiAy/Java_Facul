b2ef4c2a9fd2eb41d0b1d0519780fe0996916422
Laiz Detros <megami.ay@gmail.com>
2024-08-19 21:00:20 -0300

    Create Principal.java

---
+++ exercicio /ex05/Principal.java
@@ -0,0 +1,17 @@
+package ex05;
+
+public class Principal { 
+	
+	
+    public static void main(String[] args) {
+    		
+        for (int i = 1; i < 99; i++) {
+            if(i%3==0) {
+            	System.out.println("multiplo de 3: "+ i);
+            }
+        }
+        
+    }
+    
+    
+}
