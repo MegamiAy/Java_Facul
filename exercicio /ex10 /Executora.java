5b541cda7753fcdf2e73c8ffd0d67f7c2d7a592b
Laiz Detros <megami.ay@gmail.com>
2024-09-09 20:27:36 -0300

    Create Executora.java

---
+++ exercicio /ex10 /Executora.java
@@ -0,0 +1,13 @@
+package ex10;
+
+public class Executora {
+	
+	public static void main(String[] args) {
+		MontainBike bicMb = new MontainBike(18, 60, "anti-shock");
+		Ciclismo bicCicl = new Ciclismo(24, 80, 25);
+		
+		System.out.println("Montain Bike" + bicMb.obterDescricao());
+		System.out.println("Ciclismo" + bicCicl.obterDescricao());
+	}
+	
+}
