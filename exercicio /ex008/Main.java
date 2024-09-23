3373d381db2e5c3b617f9cda02d793a4ad64cb88
Laiz Detros <megami.ay@gmail.com>
2024-09-23 16:11:14 -0300

    Create Main.java

---
+++ exercicio /ex008/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args){
+        Funcionario func1 = new Funcionario("José", 23, 5556.99);
+        
+        System.out.println(func1); 
+    }
+    
+}
