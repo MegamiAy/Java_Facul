c9192971c9187bcbe7cc27e6cf372c5f74e55572
Laiz Detros <megami.ay@gmail.com>
2024-10-14 20:03:42 -0300

    Create Estudante.java

---
+++ db/ex10/Estudante.java
@@ -0,0 +1,15 @@
+package aula10;
+
+public class Estudante {
+	private int matrEstudante;
+	private String nomeEstudante;
+	private String cidadeEstudante;
+	
+	public Estudante(int matrEstudante, String nomeEstudante, String cidadeEstudante) {
+		this.matrEstudante = matrEstudante;
+		this.nomeEstudante = nomeEstudante;
+		this.cidadeEstudante = cidadeEstudante;
+	}
+	
+	
+}
