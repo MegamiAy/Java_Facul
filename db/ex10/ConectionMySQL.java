2b433dbf110c3c96d41d992b60f27681c0c9dd6e
Laiz Detros <megami.ay@gmail.com>
2024-10-14 20:03:07 -0300

    Create ConectionMySQL.java

---
+++ db/ex10/ConectionMySQL.java
@@ -0,0 +1,39 @@
+package aula10;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+
+public class ConectionMySQL {
+
+	public static String status = "não conectado";
+	
+	public ConectionMySQL() {
+		
+	}
+	
+	public static java.sql.Connection getConectionMySQL(){
+		Connection connection = null;
+		String driveName = "com.mysql.cj.jdbc.Driver";
+		try {
+			Class.forName(driveName);
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		String serverName = "localhost";
+		String mydatabase = "escoladb";
+		String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+		String username = "root";
+		String password = "";
+		
+		try {
+			connection = DriverManager.getConnection(url, username, serverName);
+			status = "Conectado ao DB";
+		} catch (SQLException e) {
+			System.out.println(status);
+			e.printStackTrace();
+		}
+				
+		return connection;
+	}
+}
